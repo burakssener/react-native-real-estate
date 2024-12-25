7472336d6f768dbc4306b2655b725a938686ee5c
Burak <ssenerburak@gmail.com>
2024-12-25 22:14:19 +0300

    updated main page

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
-
+import {SafeAreaView} from 'react-native-safe-area-context'
 const SignIn = () => {
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView className="flex-1"></ScrollView>
+    </SafeAreaView>
   )
 }
 
