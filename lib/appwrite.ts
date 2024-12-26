2bc4c6b9e4df2d1f8392859862abc6b8b0727149
Burak <ssenerburak@gmail.com>
2024-12-26 10:10:15 +0300

    write appwrite.ts

---
+++ lib/appwrite.ts
@@ -0,0 +1,93 @@
+import {Account ,Avatars, Client, ID, OAuthProvider} from "react-native-appwrite";
+
+import * as Linking from 'expo-linking';
+
+import {openAuthSessionAsync} from 'expo-web-browser';
+
+export const config = {
+    platform:"com.airtocltd.restate",
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECTID,
+
+}
+
+export  const client = new Client();
+
+client
+    .setEndpoint(config.endpoint!)
+    .setProject(config.projectId!)
+    .setPlatform(config.platform!);
+
+
+export const avatar = new Avatars(client);
+
+export const account = new Account(client);
+
+export async function login()
+{
+    try {
+        const redirectUrl = Linking.createURL("/");
+
+        const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUrl);
+        
+        if(!response) throw new Error('Failed to login');
+
+        const browserResult = await openAuthSessionAsync(response.toString(), redirectUrl);
+        if(browserResult.type !== 'success') throw new Error('Failed to login');
+
+        const url = new URL(browserResult.url);
+
+        const secret = url.searchParams.get('secret')?.toString();
+        const userId = url.searchParams.get('userId')?.toString();
+
+
+        if(!secret || !userId) throw new Error('Failed to login');
+
+
+        const session = await account.createSession(userId, secret);
+
+        if(!session) throw new Error('Failed to create Session');
+
+        return true;
+
+    } catch (error) {
+        console.error(error);
+        return false;
+        
+    }
+}
+
+export async function logout()
+{
+    try {
+        await account.deleteSession('current');
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+export async function getUser()
+{
+    try {
+        const response = await account.get();
+        
+        
+        if(response.$id)
+        {
+            const userAvatar = avatar.getInitials(response.name);
+
+            return {
+                ...response,
+                avatar:userAvatar.toString(),};
+        }
+        
+
+        return null;
+        
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
